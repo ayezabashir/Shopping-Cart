12db919ba22ea5f2cedbf769937411a8db88e79c
Ayeza Bashir <ayezabashir46@gmail.com>
2025-09-30 17:39:09 +0500

    headers

---
+++ src/stores/cart.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  items: localStorage.getItem("carts") ? JSON.parse(localStorage.getItem("carts")) : [],
+  statusTab: false
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const { productId, quantity } = action.payload;
+      const indexProductId = state.items.findIndex(
+        (item) => item.productId === productId
+      );
+      if (indexProductId >= 0) {
+        state.items[indexProductId].quantity += quantity;
+      } else {
+        state.items.push({ productId, quantity });
+      }
+      localStorage.setItem("carts", JSON.stringify(state.items));
+    }
+  },
+});
+export const { addToCart} = cartSlice.actions;
+export default cartSlice.reducer;

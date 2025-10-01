c1ca9226ab63802811bd32fdf013391e1578098b
Ayeza Bashir <ayezabashir46@gmail.com>
2025-10-01 21:58:15 +0500

    toggle status tab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import {products} from '../products';
+import ProductCard from '../components/ProductCard'
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-3xl my-5'>List Products</h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
-        home
+        {
+          products.map((product, key) =>
+            <>
+              <ProductCard key={key} data={product} />
+            </>
+          )
+        }
       </div>
     </div>
   )
